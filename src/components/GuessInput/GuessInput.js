829d4409767486eef98e1a960772529405573cd9
Sarah Duncan <sarah.duncan@zuto.com>
2025-07-16 16:33:02 +0100

    adds exercise 2

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function GuessInput() {
   const [guess, setGuess] = React.useState("");
+  const [previousGuesses, setPreviousGuesses] = React.useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -9,24 +10,43 @@ function GuessInput() {
       alert("Guess must be exactly 5 letters long.");
       return;
     }
-    console.log("Guess submitted:", guess);
+
+    const nextPreviousGuesses = [
+      ...previousGuesses,
+      { guess: guess, id: Math.random() },
+    ];
+    setPreviousGuesses(nextPreviousGuesses);
     setGuess("");
   };
 
   return (
-    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
-      <label htmlFor="guess-input">Enter your guess:</label>
-      <input
-        type="text"
-        id="guess-input"
-        value={guess}
-        minLength={5}
-        maxLength={5}
-        onChange={(event) => {
-          setGuess(event.target.value.toUpperCase());
-        }}
-      />
-    </form>
+    <>
+      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label htmlFor="guess-input">Enter your guess:</label>
+        <input
+          type="text"
+          id="guess-input"
+          value={guess}
+          minLength={5}
+          maxLength={5}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+        />
+      </form>
+      <div>
+        <h3>Previous guesses:</h3>
+        <div>
+          {previousGuesses.map((guess) => {
+            return (
+              <label key={guess.id} className="guess">
+                {guess.guess}
+              </label>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
