cddc5a5c7710c6b19fe4c625e11315c7dcbf41d9
Sarah Duncan <sarah.duncan@zuto.com>
2025-07-18 16:37:22 +0100

    adds exercise 5

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,31 +2,58 @@ import { checkGuess } from "../../game-helpers";
 
 function Guess({ numberOfTries, previousGuesses }) {
   const answer = "APPLE";
+  const lastGuess = previousGuesses[previousGuesses.length - 1];
+  const lastGuessResult = lastGuess ? checkGuess(lastGuess.guess, answer) : [];
+  const isFullGuessCorrect =
+    lastGuessResult.length === 5 &&
+    lastGuessResult.every((r) => r.status === "correct");
+
+  const isGameOver =
+    isFullGuessCorrect || previousGuesses.length >= numberOfTries;
 
   return (
-    <div className="guess-results">
-      {[...Array(numberOfTries)].map((_, index) => {
-        const guessObj = previousGuesses[index];
-        const guessString = guessObj ? guessObj.guess : "";
-        const guessResult = guessObj ? checkGuess(guessString, answer) : [];
+    <>
+      <div className="guess-results">
+        {[...Array(numberOfTries)].map((_, index) => {
+          const guessObj = previousGuesses[index];
+          const guessString = guessObj ? guessObj.guess : "";
+          const guessResult = guessObj ? checkGuess(guessString, answer) : [];
 
-        return (
-          <p className="guess" key={index}>
-            {guessString
-              .padEnd(5)
-              .split("")
-              .map((letter, letterIndex) => {
-                const status = guessResult[letterIndex]?.status || "";
-                return (
-                  <span key={letterIndex} className={`cell ${status}`}>
-                    {letter}
-                  </span>
-                );
-              })}
+          return (
+            <p className="guess" key={index}>
+              {guessString
+                .padEnd(5)
+                .split("")
+                .map((letter, letterIndex) => {
+                  const status = guessResult[letterIndex]?.status || "";
+                  return (
+                    <span key={letterIndex} className={`cell ${status}`}>
+                      {letter}
+                    </span>
+                  );
+                })}
+            </p>
+          );
+        })}
+      </div>
+
+      {isFullGuessCorrect && (
+        <div className="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in{" "}
+            <strong>{previousGuesses.length} guesses</strong>.
           </p>
-        );
-      })}
-    </div>
+        </div>
+      )}
+
+      {!isFullGuessCorrect && previousGuesses.length >= numberOfTries && (
+        <div className="sad banner">
+          <p>
+            Sorry, the correct answer is <strong>{answer}</strong>.
+          </p>
+        </div>
+      )}
+    </>
   );
 }
 
